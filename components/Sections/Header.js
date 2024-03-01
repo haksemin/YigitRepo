2fafab0a2931463689991a45b3096c843ad69c86
haksemin <haksemin@gmail.com>
2024-03-01 19:16:49 +0300

    bilmukabele

--- components/Sections/Header.js
+++ components/Sections/Header.js
@@ -1,22 +1,41 @@
-import { View,Dimensions,Image,StyleSheet } from "react-native"
+import React from 'react';
+import { View, Dimensions, Image, StyleSheet, Pressable } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+const { width, height } = Dimensions.get('window');
 
 
-const { width, height} = Dimensions.get("window");
 
-export default function Header(){
-    return(
-        <View style={styles.header}>
-            <Image source={require("../../assets/images/yigitlogo.png")} style={{height: height* 0.086}}></Image>
+export default function Header() {
+
+  const navigation = useNavigation();
+  
+  function goBackFunction() {
+    
+    navigation.goBack();
+ }
+ 
+  
+  return (
+    <View style={styles.header}>
+      {navigation.canGoBack() ? (
+        <View style={{ position: 'absolute', left: 15 }}>
+          <Pressable onPress={goBackFunction}>
+            <Image source={require('../../assets/images/ikon/BackIcon.png')} style={{ height: height * 0.03, width: width * 0.05 }} />
+          </Pressable>
         </View>
-    )
+      ) : null}
+      <Image source={require('../../assets/images/yigitlogo.png')} style={{ height: height * 0.086, resizeMode: 'contain' }} />
+    </View>
+  );
 }
 
-
 const styles = StyleSheet.create({
-    header: {
-      backgroundColor: "#FBBC0F",
-      height: height * 0.086,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-  });
+  header: {
+    backgroundColor: '#FBBC0F',
+    height: height * 0.086,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row', 
+  },
+});
