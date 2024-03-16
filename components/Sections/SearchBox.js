000f3c8aa4ce1c2eafbbf969821bf9cee2b0b569
haksemin <haksemin@gmail.com>
2024-03-16 15:36:12 +0300

    searchbox

--- components/Sections/SearchBox.js
+++ components/Sections/SearchBox.js
@@ -9,14 +9,17 @@ const windowHeight = Dimensions.get('window').height;
 
 export default function SearchBox({ onChangeText }) {
     return (
-      <View style={{width: Dimensions.get('window').width * 0.9, alignSelf: "center", marginVertical: 15}}>
+      <View style={{width: Dimensions.get('window').width * 0.9, alignSelf: "center", marginVertical: 15,flexDirection:"row"}}>
+        
         <TextInput 
-          placeholder="Ürün Ara"
+          placeholder=" 🔍 Ürün Ara 🎁"
           clearButtonMode="always"
           autoCapitalize="none"
           autoCorrect={false}
           onChangeText={onChangeText}
-          style={{borderColor: "grey", borderWidth: 1, borderRadius: 8, paddingVertical: 10, paddingHorizontal: 5, backgroundColor: "white"}}></TextInput>
+          style={{borderColor: "grey", borderWidth: 0.5, borderRadius:12, paddingVertical: 10, paddingHorizontal: 5,flex:1,backgroundColor:"white",shadowOpacity:5,shadowOffset:{width:15,height:5},elevation:5 }}>
+            
+          </TextInput>
       </View>
     );
   }
